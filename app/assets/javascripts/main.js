bd9fdaa9110bbf51b0ad7d74be86833d94bfdd17
Jake Austin <jakeaust07@gmail.com>
2014-07-23 10:07:55 -0400

    todos can be created with form, displayed on home page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   var getTodos = function() {
-    console.log('in getTodos');
     $.ajax({
       url: "http://localhost:3000/todos",
       type: "GET",
@@ -16,13 +15,48 @@ $(document).ready(function() {
     var todosHTML = '';
     //build html
     for(var i=0; i<todos.length; i++){
-      todosHTML+= '<li class="uncomplete">' + todos[i].content + '</li>';
+      todosHTML+= '<li class="uncomplete">';
+      todosHTML+= todos[i].content + ' -- ';
+      todosHTML+= todos[i].created_at;
+      todosHTML+= '</li>';
     }
 
     $('#todos').empty();
     $('#todos').append(todosHTML);
   };
 
+  todoHTML = function(todo){
+    var html = '<li class="uncomplete">';
+    html+= todo.content + ' -- ';
+    html+= todo.created_at;
+    html+= '</li>';
+    return html;
+  },
+  getTodo = function(todo){
+    // Add the article to the article list
+    $('#todos').append(todoHTML(todo));
+  },
+  createTodoCallbackHandler = function(event){
+    var $form = $(event.target);
+    var $content = $form.find("input[name='content']");
+    var action = $form.attr('action');
+    var requestObj = {todo: {content: $content.val()}};
+
+    event.preventDefault();
+
+    //create and send POST
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/todos',
+      data: requestObj,
+      dataType: 'json'
+    }).done(getTodo);
+
+    $content.val('');
+  };
+
+// Set up click handler for form submit
+  $('#new-todo').on('submit', createTodoCallbackHandler);
   // Set up click handler for getting articles.
   $('#get-todos').on('click', getTodos);
   // Simulate a user click event.
